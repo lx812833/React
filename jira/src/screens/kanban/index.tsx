54f534143816c374f65726034ccb5147ede9edb3
龙 富阳 <lx812833@163.com>
2021-06-17 17:32:34 +0800

    react实现类似插槽功能

--- jira/src/screens/kanban/index.tsx
+++ jira/src/screens/kanban/index.tsx
@@ -1,5 +1,21 @@
-export const KanbanScreen = () => {
-  return <h1>
-    看板
-  </h1>
+import React from 'react';
+/**
+ * React.ReactNode: 是一种联合类型(Union Types);
+ * type ReactNode = ReactChild | ReactFragment | ReactPortal | boolean | null | undefined;
+ */
+interface CardProps {
+  title?: string,
+  renderTitle?: Function,
+  children: React.ReactNode
+}
+
+export const KanbanScreen = (props: CardProps) => {
+  const { renderTitle } = props
+
+  let titleRen = renderTitle ? renderTitle() : null
+  return <div>
+    <h2>{props.title}</h2>
+    {titleRen}
+    <div>{props.children}</div>
+  </div>
 }
--- jira/src/screens/project/index.tsx
+++ jira/src/screens/project/index.tsx
@@ -9,7 +9,15 @@ export const ProjectScreen = () => {
     <Link to="kanban">看板</Link>
     <Link to="task">任务组</Link>
     <Routes>
-      <Route path={"kanban"} element={<KanbanScreen />} />
+      <Route path={"kanban"} element={
+        <KanbanScreen title="标题测试" renderTitle={() => {
+          return <p>
+            这里是通过render函数渲染出来的。
+          </p>
+        }}>
+          <div>这是传递的React.ReactNode</div>
+        </KanbanScreen>}
+      />
       <Route path={"task"} element={<TaskScreen />} />
       <Navigate to={window.location.pathname + "/kanban"} />
     </Routes>

fdf167ab418e3ea1d6ca16ded9568c8d1ee3ba5e
longfuyang <lx812833@163.com>
2021-07-27 23:31:05 +0800

    安装react-router

--- travel/src/App.tsx
+++ travel/src/App.tsx
@@ -1,46 +1,10 @@
-import { Col, Row, Typography } from "antd";
+import { HomePage } from "./views/index";
 import styles from "./App.module.css";
-import { Header, Footer, SideMenu, Carousel, ProductCollection } from "./components/index";
-import { productList1, productList2, productList3 } from "./assets/mock";
-import sideImage from "./assets/images/sider_2019_12-09.png";
-import sideImage2 from "./assets/images/sider_2019_02-04.png";
-import sideImage3 from "./assets/images/sider_2019_02-04-2.png";
 
 function App() {
   return (
     <div className={styles.App}>
-      <Header />
-
-      <div className={styles['page-content']}>
-        <Row style={{ paddingTop: 20 }}>
-          <Col span={6}>
-            <SideMenu />
-          </Col>
-          <Col span={18}>
-            <Carousel />
-          </Col>
-        </Row>
-
-        <ProductCollection
-          title={<Typography.Title level={3} type="warning">爆款推荐</Typography.Title>}
-          sideImage={sideImage}
-          products={productList1}
-        />
-
-        <ProductCollection
-          title={<Typography.Title level={3} type="danger">新品上市</Typography.Title>}
-          sideImage={sideImage2}
-          products={productList2}
-        />
-
-        <ProductCollection
-          title={<Typography.Title level={3} type="success">国内游推荐</Typography.Title>}
-          sideImage={sideImage3}
-          products={productList3}
-        />
-      </div>
-      
-      <Footer />
+      <HomePage />
     </div>
   );
 }
---
+++ travel/src/views/home/HomePage.tsx
@@ -0,0 +1,46 @@
+import { Col, Row, Typography } from "antd";
+import styles from "./HomePage.module.css";
+import { Header, Footer, SideMenu, Carousel, ProductCollection } from "../../components/index";
+import { productList1, productList2, productList3 } from "../../assets/mock";
+import sideImage from "../../assets/images/sider_2019_12-09.png";
+import sideImage2 from "../../assets/images/sider_2019_02-04.png";
+import sideImage3 from "../../assets/images/sider_2019_02-04-2.png";
+
+export const HomePage = () => {
+  return (
+    <>
+      <Header />
+
+      <div className={styles['page-content']}>
+        <Row style={{ paddingTop: 20 }}>
+          <Col span={6}>
+            <SideMenu />
+          </Col>
+          <Col span={18}>
+            <Carousel />
+          </Col>
+        </Row>
+
+        <ProductCollection
+          title={<Typography.Title level={3} type="warning">爆款推荐</Typography.Title>}
+          sideImage={sideImage}
+          products={productList1}
+        />
+
+        <ProductCollection
+          title={<Typography.Title level={3} type="danger">新品上市</Typography.Title>}
+          sideImage={sideImage2}
+          products={productList2}
+        />
+
+        <ProductCollection
+          title={<Typography.Title level={3} type="success">国内游推荐</Typography.Title>}
+          sideImage={sideImage3}
+          products={productList3}
+        />
+      </div>
+
+      <Footer />
+    </>
+  )
+}

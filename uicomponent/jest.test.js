d61988bfd4bad75cb2f1242030ed99d367d42a3e
longfuyang <lx812833@163.com>
2021-09-28 22:42:06 +0800

    通用测试框架Jest

---
+++ uicomponent/jest.test.js
@@ -0,0 +1,15 @@
+// npx jest jest.test.js --watch
+
+test("test common matcher", () => {
+  expect(2 + 2).toBe(4)
+  expect(2 + 2).not.toBe(5)
+})
+
+test("test to be true or false", () => {
+  expect(1).toBeTruthy()
+  expect(0).toBeFalsy()
+})
+
+test("test object", () => {
+  expect({ name: "llr" }).toEqual({ name: "llr" })
+})

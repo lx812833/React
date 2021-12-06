1fb6d90ecfbb8973be680bb44f3f2030ba390f21
longfuyang <lx812833@163.com>
2021-12-06 22:38:21 +0800

    ReactDOM.flushSync

--- uicomponent/src/example/JSXPractice/index.tsx
+++ uicomponent/src/example/JSXPractice/index.tsx
@@ -1,4 +1,6 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import ReactDOM from 'react-dom';
+import { setTimeout } from "timers";
 
 interface ChildProps {
   fatherSay: string;
@@ -27,13 +29,32 @@ export const JSXPractice: React.FC = (props) => {
 export const Parent: React.FC = (props) => {
   const [childSay, setchildSay] = useState("");
   const [fatherSay, setFatherSay] = useState("");
+  const [number, setNumber] = useState(0);
+
+  useEffect(() => {
+    console.log('监听number变化，此时的number是:  ' + number);
+  }, [number])
+
   const handleToChild = (value: string) => {
     setFatherSay(value);
+
+    // 高优先级更新
+    ReactDOM.flushSync(() => {
+      setNumber(1);
+    })
+
+    // 批量更新
+    setNumber(2);
+
+    // 滞后更新，批量更新则被打破
+    setTimeout(() => {
+      setNumber(3) 
+    }, 0)
   }
 
   return (
     <div style={{ marginTop: "50px" }}>
-      父组件：
+      父组件：<span>{number}</span>
       <div>子组件对父组件说： {childSay}</div>
       <input type="text" onChange={e => handleToChild(e.target.value)} placeholder="父对子说：" />
       <Child fatherSay={fatherSay} childSay={setchildSay} />

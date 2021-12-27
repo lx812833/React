70206fa7b3c131c6cee356154c691dc0bdede523
longfuyang <lx812833@163.com>
2021-12-27 17:31:49 +0800

    配置css module

--- uicomponent/src/react-app-env.d.ts
+++ uicomponent/src/react-app-env.d.ts
@@ -1,1 +1,8 @@
 /// <reference types="react-scripts" />
+declare module "*.module.scss" {
+  const classes: {
+    readonly [key: string]: string
+  }
+  export default classes
+  declare module "*.scss"
+}

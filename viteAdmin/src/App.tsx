6db54f56e8615258cf6253f7b7d9b9208c112f2b
longfuyang <longfuyang@aosign.cn>
2022-09-16 13:37:10 +0800

    feat：初始化配置

--- viteAdmin/src/App.tsx
+++ viteAdmin/src/App.tsx
@@ -1,9 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.less";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <div className="App">

147715bb474b976ff1e26b4c401bbde6a022d76c
longfuyang <longfuyang@aosign.cn>
2022-09-19 15:46:14 +0800

    feat：登录接口

--- viteAdmin/src/views/login/components/loginForm.tsx
+++ viteAdmin/src/views/login/components/loginForm.tsx
@@ -1,17 +1,14 @@
-import md5 from "js-md5";
 import { useState } from "react";
 import { Button, Form, Input, message } from "antd";
 import { useNavigate } from "react-router-dom";
-import { loginApi } from "@/api/modules/login";
-
-import { HOME_URL } from "@/config/config";
-import { connect } from "react-redux";
-// import { setToken } from "@/redux/modules/global/action";
 import { useTranslation } from "react-i18next";
-// import { setTabsList } from "@/redux/modules/tabs/action";
+import md5 from "js-md5";
+import { loginApi, Login } from "@/api/modules/login";
+import { HOME_URL } from "@/config/config";
 import { UserOutlined, LockOutlined, CloseCircleOutlined } from "@ant-design/icons";
-import { Login } from "@/api/modules/login";
-
+import { connect } from "react-redux";
+import { setToken as setTokenDispatch } from "@/redux/modules/global/action";
+import { setTabsList as setTabsListDispatch } from "@/redux/modules/tabs/action";
 
 const LoginForm = (props: any) => {
 	const { t } = useTranslation();
@@ -25,14 +22,11 @@ const LoginForm = (props: any) => {
 		try {
 			setLoading(true);
 			loginForm.password = md5(loginForm.password);
-			// const { data } = await loginApi(loginForm);
-			const res = await loginApi(loginForm);
-			console.log("登录的res", res);
-
-			// setToken(data?.access_token);
-			// setTabsList([]);
-			// message.success("登录成功！");
-			// navigate(HOME_URL);
+			const { data } = await loginApi(loginForm);
+			setToken(data?.access_token);
+			setTabsList([]);
+			message.success("登录成功！");
+			navigate(HOME_URL);
 		} finally {
 			setLoading(false);
 		}
@@ -54,10 +48,10 @@ const LoginForm = (props: any) => {
 			autoComplete="off"
 		>
 			<Form.Item name="username" rules={[{ required: true, message: "请输入用户名" }]}>
-				<Input placeholder="用户名：admin / user" defaultValue="admin" prefix={<UserOutlined />} />
+				<Input placeholder="用户名：admin / user" prefix={<UserOutlined />} />
 			</Form.Item>
 			<Form.Item name="password" rules={[{ required: true, message: "请输入密码" }]}>
-				<Input.Password autoComplete="new-password" defaultValue="123456" placeholder="密码：123456" prefix={<LockOutlined />} />
+				<Input.Password autoComplete="new-password" placeholder="密码：123456" prefix={<LockOutlined />} />
 			</Form.Item>
 			<Form.Item className="login-btn">
 				<Button
@@ -77,5 +71,6 @@ const LoginForm = (props: any) => {
 };
 
 export default LoginForm;
-// const mapDispatchToProps = { setToken, setTabsList };
+
+// const mapDispatchToProps = { setTokenDispatch, setTabsListDispatch };
 // export default connect(null, mapDispatchToProps)(LoginForm);

f1ec95c6d9d99d1dc773b7787c3104e8c86efca4
longfuyang <longfuyang@aosign.cn>
2022-09-19 14:13:24 +0800

    feat：ConfigProvider+react-i18next设置多语言

--- viteAdmin/src/App.tsx
+++ viteAdmin/src/App.tsx
@@ -1,11 +1,36 @@
+import { useEffect, useState } from "react";
 import { HashRouter } from "react-router-dom";
+import { ConfigProvider } from "antd";
 import Router from "@/routers/index";
+import i18n from "i18next";
+import { getBrowserLang } from "@/utils/util";
+import zhCN from "antd/lib/locale/zh_CN";
+import enUS from "antd/lib/locale/en_US";
 
-function App() {
+function App(props: any) {
+  const { language } = props;
+  const [i18nLocale, setI18nLocale] = useState(enUS);
+
+  const setAntdLanguage = () => {
+    // 如果 redux 中有默认语言就设置成 redux 的默认语言，没有默认语言就设置成浏览器默认语言
+    if (language && language == "zh") return setI18nLocale(zhCN);
+    if (language && language == "en") return setI18nLocale(enUS);
+    if (getBrowserLang() == "zh") return setI18nLocale(zhCN);
+    if (getBrowserLang() == "en") return setI18nLocale(enUS);
+  };
+
+  useEffect(() => {
+    i18n.changeLanguage(language || getBrowserLang()); // 全局使用国际化
+    // setLanguage(language || getBrowserLang());
+    setAntdLanguage();
+  }, [language]);
+
   return (
     <HashRouter>
-      <Router />
-    </HashRouter>
+      <ConfigProvider locale={i18nLocale}>
+        <Router />
+      </ConfigProvider>
+    </HashRouter >
   )
 }
 
--- viteAdmin/src/main.tsx
+++ viteAdmin/src/main.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "@/App";
+import "@/language/index";
 import "@/styles/common.less";
 import "@/styles/reset.less";
 import "@/assets/iconfont/iconfont.less";
 import "@/assets/fonts/font.less";
+import "antd/dist/antd.less";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>

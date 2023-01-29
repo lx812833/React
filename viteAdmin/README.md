881ee420730f22fda4f351e58310c89fe50ef809
longfuyang <longfuyang@aosign.cn>
2023-01-29 16:46:56 +0800

    build：Immer

--- viteAdmin/README.md
+++ viteAdmin/README.md
@@ -1,3 +1,6 @@
+基于 React18、React-Router v6、React-Hooks、Redux && Redux-Toolkit、TypeScript、Vite2、Ant-Design 开源的一套后台管理框架
+https://juejin.cn/post/7114555646820745253
+
 https://juejin.cn/post/7059236538923614216
 
 Geeker-Admin
--- viteAdmin/src/redux/modules/tabs/reducer.ts
+++ viteAdmin/src/redux/modules/tabs/reducer.ts
@@ -8,6 +8,10 @@ const tabsState: TabsState = {
 	tabsList: [{ title: "首页", path: HOME_URL }]
 };
 
+// Immer 解决了什么问题？
+// 解决了在 React 当中强调的不可变数据 (immutable) 的性能问题，可以不使用深拷贝 这样浪费性能的操作来完成这一过程。
+// Immer 原理：Immer 通过 递归式的 proxy 对象代理 和 浅拷贝，提高了不可变数据的性能，尽可能的复用数据结构当中其他节点的内存。既满足了性能要求，又使得数据达到了不可变数据的要求。
+
 // tabs reducer
 const tabs = (state: TabsState = tabsState, action: AnyAction) =>
 	produce(state, draftState => {

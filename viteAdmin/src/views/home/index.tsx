edb291f789092180ae01cc95a2a7cab5572a6910
longfuyang <longfuyang@aosign.cn>
2023-03-20 11:36:22 +0800

    feat：父组件调用子组件方法并回调父组件里的方法

--- viteAdmin/src/views/home/index.tsx
+++ viteAdmin/src/views/home/index.tsx
@@ -1,11 +1,39 @@
+import { forwardRef, useImperativeHandle, useRef } from "react";
 import welcome from "@/assets/images/welcome01.png";
 import "./index.less";
 
+const Child = forwardRef((_, ref): any => {
+	useImperativeHandle(ref, () => {
+		return {
+			propsEvent: (val: string, func: Function) => handleTest(val, func)
+		}
+	})
+
+	const handleTest = (val: string, func: Function) => {
+		console.log('传递的', val);
+
+		setTimeout(() => {
+			func('321');
+		}, 1500);
+	}
+})
+
 const Home = () => {
+	const childRef: any = useRef();
+
+	const handleClick = () => {
+		childRef.current.propsEvent('123', handleBack);
+	}
+
+	const handleBack = (val: string) => {
+		console.log('回调了', val);
+	}
+
 	return (
 		<div className="home card">
-			这是home页
-			{/* <img src={welcome} alt="welcome" /> */}
+			<img src={welcome} alt="welcome" onClick={handleClick} />
+
+			<Child ref={childRef} />
 		</div>
 	);
 };
